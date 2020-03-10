410f3d049616a210c61516ff18d1ad9ae963f91e
wanatchaya.bo.61@ubu.ac.th <wilawan.ku.61@ubu.ac.th>
2020-03-10 13:35:30 +0700

    wilawanmild

---
+++ ProjectDatascience/chatgroup/static/train.py
@@ -0,0 +1,21 @@
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import make_pipeline
+from joblib import dump
+
+data = fetch_20newsgroups()
+
+categories = ['comp.sys.mac.hardware', 'misc.forsale', 'sci.electronics', 'rec.motorcycles']
+train = fetch_20newsgroups(subset='train', categories=categories)
+test = fetch_20newsgroups(subset='test', categories=categories)
+model = make_pipeline(TfidfVectorizer(), MultinomialNB())
+model.fit(train.data, train.target)
+labels = model.predict(test.data)
+test.target[0:10]
+n = len(test.data)
+corrects = [ 1 for i in range(n) if test.target[i] == labels[i] ]
+print(n)
+print(sum(corrects))
+print(sum(corrects)*100/n)
+dump(model, 'chatgroup.model')
